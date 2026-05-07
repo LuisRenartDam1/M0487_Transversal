543432054331e8e72592c7e61f40974d5578c5af
Joel del Rio <joel.inttra2@gmail.com>
2026-05-07 09:40:23 +0200

    24 - creacion de la tabla productos

--- MODEL/BBDDTransversal.sql
+++ MODEL/BBDDTransversal.sql
@@ -3,8 +3,16 @@ create database BBDDTransversal;
 use BBDDTransversal;
 
 create table users(
-	username varchar(30) primary key,
-    password varchar(100) 
+	username varchar(30) primary key not null,
+    password varchar(100) not null 
+);
+
+create table productos(
+    id_product int auto_increment not null,
+    name varchar(100) not null,
+    price int not null,
+    amount int not null,
+    stock boolean not null
 );
 
 
